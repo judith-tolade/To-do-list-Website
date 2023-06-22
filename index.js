0807717dfe0e24184fcfb58774648fd2daca9e34
judith-tolade <101555581+judith-tolade@users.noreply.github.com>
2023-06-22 16:13:05 +0100

    Add the load function to the JS
    This was added to stop the page from refreshing after the task has been filled.

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+  let form = document.querySelector("#task-form");
+  let input = document.querySelector("#task-input");
+  let list_el = document.querySelector("#tasks");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+
+})
+
+})
