df9b1c12db1e7e4fd162951286db44f0cf1220f0
judith-tolade <101555581+judith-tolade@users.noreply.github.com>
2023-06-22 16:36:12 +0100

    Added the edit and delete input

--- index.js
+++ index.js
@@ -12,9 +12,33 @@ form.addEventListener("submit", (e) => {
 
     const task_content_el = document.createElement("div");
     task_content_el.classList.add("content");
-    task_content_el.innerText = task;
 
     task_el.appendChild(task_content_el);
+    // Adding the edit and delete button
+    const task_input_el = document.createElement("input");
+    task_input_el.classList.add("text");
+    task_input_el.type = "text";
+    task_input_el.value = task;
+    task_input_el.setAttribute("readonly", "readonly");
+
+    task_content_el.appendChild(task_input_el);
+
+    const task_actions_el = document.createElement("div");
+    task_actions_el.classList.add("actions");
+
+    const task_edit_el = document.createElement("button");
+    task_edit_el.classList.add("edit");
+    task_edit_el.innerText = "Edit";
+
+    const task_delete_el = document.createElement("button");
+    task_delete_el.classList.add("delete");
+    task_delete_el.innerText = "Delete";
+
+    task_actions_el.appendChild(task_edit_el);
+    task_actions_el.appendChild(task_delete_el);
+
+    task_el.appendChild(task_actions_el);
+
     list_el.appendChild(task_el);
 
 })
