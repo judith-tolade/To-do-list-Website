9f80dae4e87f3a16bd3e0848f65c42ebcf96337c
judith-tolade <101555581+judith-tolade@users.noreply.github.com>
2023-06-22 16:25:15 +0100

    Enabled the add task button

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ window.addEventListener("load", () => {
 
 form.addEventListener("submit", (e) => {
     e.preventDefault(); 
+    const task = input.value;
+
+    const task_el = document.createElement("div");
+    task_el.classList.add("task");
+
+    const task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+    task_content_el.innerText = task;
+
+    task_el.appendChild(task_content_el);
+    list_el.appendChild(task_el);
 
 })
 
